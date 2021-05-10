4e2c16344cb61684cd77991a5973350e2c5aae64
mekala15 <66716327+mekala15@users.noreply.github.com>
2021-05-10 13:41:05 +0530

    Day4

--- MyTask.java
+++ MyTask.java
@@ -1,42 +1,44 @@
-public class MyTask
-{
-	String myFirst(String s,char f,char new)
+public class MyTask {
+	String myFirst(String s,char f,char n)
 	{
                 int i;
-		char[] arr= data.toCharArray();
+		char[] arr= s.toCharArray();
 		for(i=0;i<arr.length;i++)
 		{
 			if(arr[i]==f)
 			{
-				arr[i]=new;
+				arr[i]=n;
 				break;
 			}
 		}
 		String res=new String(arr);
 		return  res;
 	}
-	String myLast(String s,char l,char new)
+	String myLast(String s,char l,char n)
 	{
                 int i;
-		char[] arr= data.toCharArray();
+		char[] arr= s.toCharArray();
 		for(i=arr.length-1;i>=0;i--)
 		{
-			if(ar[i]==l)
+			if(arr[i]==l)
 			{
-				arr[i]=new;
+				arr[i]=n;
 				break;
 			}
 		}
 		String res=new String(arr);
 		return  res;
 	}
-public static void main(String[] args) 
-{
-	String str="HELLO";
-	MyTask m=new MyTask();
-	String str1= m.myFirst(str, 'L', 'Y');
-	String str2= m.myLast(str, 'L', 'Y');
-	System.out.println(str1);
-	System.out.println(str2);
-}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="HELLO";
+		MyTask m=new MyTask();
+		String str1= m.myFirst(str, 'L', 'Y');
+		String str2= m.myLast(str, 'L', 'Y');
+		System.out.println(str1);
+		System.out.println(str2);
+
+	}
+
 }
